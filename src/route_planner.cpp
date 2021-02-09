53d454409757704d7f2d541b097432cecedfd0d8
Guisoni <guisoni@yahoo.com.br>
2021-02-09 11:50:09 -0300

    Implement the CalculateHValue method

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -21,8 +21,9 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
 // - Node objects have a distance method to determine the distance to another node.
 
 float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
-
+    return node->distance(*this->end_node);
 }
+
 
 
 // TODO 4: Complete the AddNeighbors method to expand the current node by adding all unvisited neighbors to the open list.
